70612de5481beb1e2f07ce92cac0c89957a0b235
alyonaborodina <ms.borodinaa@mail.ru>
2017-04-30 18:17:47 +0500

    Core9

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core9.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Core9.Models.Product a)
+        {
+            if (ModelState.IsValid)
+            {
+                Core9.Models.ProductContext.Products.Add(new Models.Product()
+                {
+                    Name = a.Name,
+                    Count = a.Count,
+                    Date = a.Date
+                });
+                return Redirect("Index");
+            }
+            return View(a);
+        }
+
+
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Core9.Models
+{
+    public class Product
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Count { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+    }
+
+    public class ProductContext
+    {
+        public static List<Product> Products = new List<Product>();
+    }
+}
